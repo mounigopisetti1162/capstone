5229ed0e63f014e64bd1c83782dcfb164eed8d49
mouni <mounika.g1162@gmail.com>
2023-03-15 13:39:22 +0530

    backend online

--- capstone1/src/loginandsignup/global.jsx
+++ capstone1/src/loginandsignup/global.jsx
@@ -1,2 +1,3 @@
 // export const API='https://capstone-five-phi.vercel.app'
-export const API='http://localhost:4000'
+// export const API='http://localhost:4000'
+export const API='https://socket-server-api.onrender.com'
